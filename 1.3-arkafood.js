c73ec50ba935db1b0e4559676210fcf145333220
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-03 00:42:33 +0700

    update algoritma dan flowchart

---
+++ 1.3-arkafood.js
@@ -0,0 +1,28 @@
+const ArkFood = (harga, voucher, jarak, pajak) => {
+	if (voucher !== false) {
+		
+		if (voucher === 'ARKAFOOD5' &&  harga >= 50000) {
+			let diskon = harga * (50/100)
+			potongan = diskon >= 50000 ? 50000 : diskon
+
+		} else if (voucher === 'DITRAKTIRDEMY' &&  harga >= 25000) {
+			let diskon = harga * (60/100)
+			potongan = diskon >= 30000 ? 30000 : diskon
+
+		} else {
+			potongan = 0
+
+		}
+	} else {
+		potongan = 0
+	}
+	 
+	let ongkirNext = 5000 + ( (jarak - 2) * 3000 )
+	let ongkir = jarak <= 2 ? 5000 : ongkirNext
+	let tarifPajak = pajak === true ? harga * (5/100) : 0
+	let subtotal = (harga - potongan + ongkir) + tarifPajak
+
+	console.log(`Harga 		: ${harga} \nPotongan 	: ${potongan} \nBiaya Antar 	: ${ongkir} \nPajak 		: ${tarifPajak} \nSubotal 	: ${subtotal}`)
+}
+
+ArkFood(75000, 'ARKAFOOD5', 5, true)
