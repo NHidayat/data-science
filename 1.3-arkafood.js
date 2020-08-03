ec891eeb09f53001653c0946c36d58e133a7af80
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-03 18:34:17 +0700

    tambah funtion getPotongan

--- 1.3-arkafood.js
+++ 1.3-arkafood.js
@@ -1,4 +1,17 @@
+ArkFood(75000, 'ARKAFOOD5', 5, true)
 const ArkFood = (harga, voucher, jarak, pajak) => {
+	
+	let potongan = getPotongan(harga, voucher)
+	let ongkirNext = 5000 + ( (jarak - 2) * 3000 )
+	let ongkir = jarak <= 2 ? 5000 : ongkirNext
+	let tarifPajak = pajak === true ? harga * (5/100) : 0
+	let subtotal = (harga - potongan + ongkir) + tarifPajak
+
+	console.log(`Harga 		: ${harga} \nPotongan 	: ${potongan} \nBiaya Antar 	: ${ongkir} \nPajak 		: ${tarifPajak} \nSubotal 	: ${subtotal}`)
+}
+
+const getPotongan = (harga, voucher) => {
+	let potongan = ''
 	if (voucher !== false) {
 		
 		if (voucher === 'ARKAFOOD5' &&  harga >= 50000) {
@@ -16,13 +29,7 @@ const ArkFood = (harga, voucher, jarak, pajak) => {
 	} else {
 		potongan = 0
 	}
-	 
-	let ongkirNext = 5000 + ( (jarak - 2) * 3000 )
-	let ongkir = jarak <= 2 ? 5000 : ongkirNext
-	let tarifPajak = pajak === true ? harga * (5/100) : 0
-	let subtotal = (harga - potongan + ongkir) + tarifPajak
 
-	console.log(`Harga 		: ${harga} \nPotongan 	: ${potongan} \nBiaya Antar 	: ${ongkir} \nPajak 		: ${tarifPajak} \nSubotal 	: ${subtotal}`)
+	return potongan
 }
 
-ArkFood(75000, 'ARKAFOOD5', 5, true)
